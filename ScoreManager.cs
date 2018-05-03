1c1314d39a1a75440857838a58fb70de09e80044
TunaPigeon <37885031+TunaPigeon@users.noreply.github.com>
2018-05-03 15:08:05 +1200

    Score Manager

---
+++ ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoreText;
+	public Text hiScoreText;
+
+	public float scoreCount;
+	public float hiScoreCount;
+
+	public float pointsPerSecond;
+
+	//public float gameTime;
+
+	public bool scoreIncreasing;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        
+
+		scoreCount += pointsPerSecond * Time.deltaTime;
+
+		if (scoreCount > hiScoreCount)
+		{
+			hiScoreCount = scoreCount;
+		}
+
+		scoreText.text = "Score: " + Mathf.Round(scoreCount);
+		hiScoreText.text = "Highscore: " + Mathf.Round(hiScoreCount);
+		
+	}
+}
