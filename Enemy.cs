98bcc7537c15b2487deb41438221e0f7106cbc12
TunaPigeon <37885031+TunaPigeon@users.noreply.github.com>
2018-05-03 18:11:12 +1200

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    public LayerMask enemyMask;
+    public float speed;
+    Rigidbody2D mybody;
+    Transform myTrans;
+    float myWidth;
+
+	// Use this for initialization
+	void Start () {
+
+        myTrans = this.transform;
+        mybody = this.GetComponent<Rigidbody2D>();
+        myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Check to see if theres ground in front before moving forward
+        Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
+        Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+
+
+
+        //Always move forward
+        Vector2 myVel = mybody.velocity;
+        myVel.x = -speed;
+        mybody.velocity = myVel;
+	}
+}
