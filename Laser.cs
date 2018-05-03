687d8c3f1925f94ed7c8cb53e2d5ee7e91dad4e1
SakuraiSensei <37196965+SakuraiSensei@users.noreply.github.com>
2018-05-03 13:19:50 +1200

    Add files via upload

---
+++ Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private float speed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move laser upwards with the spd of 10 and current real time...
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+        //delete game object when out of page...
+        if (transform.position.x > 9f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
